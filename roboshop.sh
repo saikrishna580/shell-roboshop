4d2d150d13f6dc883c8aa7d66435801757acef38
saikowru <sai@devops.com>
2026-02-16 01:31:04 +0530

    colors

--- roboshop.sh
+++ roboshop.sh
@@ -8,12 +8,12 @@ DOMAIN_NAME="daw84s.store"
 
 for instance in ${INSTANCES[@]}
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t2.micro --security-group-ids sg-01ea0ba910221b5e4 --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=$INSTANCE}]" --query "Instances[0].InstanceId" --output text)
-    if [ $INSTANCE != "frontend"]
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t2.micro --security-group-ids sg-01ea0ba910221b5e4 --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend"]
     then
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Instances[0].PrivateIpAddress" --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     else 
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Instances[0].PublicIpAddress" --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
     fi
     echo "$instance IP address: $IP"    
 done
